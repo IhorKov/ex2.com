67fe93615c6dfeb1be9fa08b5df9c5e8079a7342
IhorKov <apasniy1234@gmail.com>
2021-08-19 20:31:23 +0300

    Add task18

---
+++ src/main/java/task18/Main.java
@@ -0,0 +1,32 @@
+package task18;
+
+import org.apache.commons.math3.primes.Primes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String checkPrimeStr = reader.readLine();
+
+        printPrime(checkPrimeStr);
+    }
+
+    public static void printPrime(String str) {
+        String[] strArr = str.split(" ");
+
+        for (int i = 0; i < strArr.length; i++) {
+            try {
+                if (Primes.isPrime(Integer.parseInt(strArr[i]))) {
+                    System.out.println(strArr[i] + " - prime");
+                } else {
+                    System.out.println(strArr[i] + " - not prime");
+                }
+            } catch (Exception e) {
+                System.out.println(strArr[i] + " - NOT A NUMBER");
+            }
+        }
+    }
+}
