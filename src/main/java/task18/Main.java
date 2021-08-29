2d40c940c629a1056d3e94191fbd0aa64d0b2e06
IhorKov <apasniy1234@gmail.com>
2021-08-29 22:30:59 +0300

    Add fix

--- src/main/java/task18/Main.java
+++ src/main/java/task18/Main.java
@@ -2,30 +2,24 @@ package task18;
 
 import org.apache.commons.math3.primes.Primes;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String checkPrimeStr = reader.readLine();
-
-        printPrime(checkPrimeStr);
+    public static void main(String[] args){
+        printPrime(args);
     }
 
-    public static void printPrime(String str) {
-        String[] strArr = str.split(" ");
+    public static void printPrime(String[] arrStr) {
 
-        for (int i = 0; i < strArr.length; i++) {
+        for (int i = 0; i < arrStr.length; i++) {
             try {
-                if (Primes.isPrime(Integer.parseInt(strArr[i]))) {
-                    System.out.println(strArr[i] + " - prime");
+                if (Primes.isPrime(Integer.parseInt(arrStr[i]))) {
+                    System.out.println(arrStr[i] + " - prime");
                 } else {
-                    System.out.println(strArr[i] + " - not prime");
+                    System.out.println(arrStr[i] + " - not prime");
                 }
             } catch (NumberFormatException e) {
-                System.out.println(strArr[i] + " - NOT A NUMBER");
+                System.out.println(arrStr[i] + " - NOT A NUMBER");
             }
         }
     }
